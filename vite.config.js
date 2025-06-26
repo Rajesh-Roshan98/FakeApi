16129767fd1cf24a4725692fe969a07015a27c0f
Rajesh-Roshan98 <rrajeshroshan89@gmail.com>
2025-06-26 21:19:04 +0530

    FakeApi Is Done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Use relative paths for GitHub Pages root deployment
   plugins: [tailwindcss(),react()],
 })
